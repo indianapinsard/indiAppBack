f8d640049cca9fea58405387aad290d0a54aa5c6
Indi <indiana.pinsard@wattys-conseil.com>
2021-02-26 17:32:31 +0100

    add deploy to travis file'

--- indi_app_back/pull-request.sh
+++ indi_app_back/pull-request.sh
@@ -1,7 +1,8 @@
 # Open the Pull Request URL for your current directory's branch (base branch defaults to staging)
 function openpr() {
   github_url=`git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#https://#' -e 's@com:@com/@' -e 's%\.git$%%' | awk '/github/'`;
-  branch_name=`git symbolic-ref HEAD | cut -d"/" -f 3,4`;
+  #branch_name=`git symbolic-ref HEAD | cut -d"/" -f 3,4`;
+  branch_name="";
   pr_url=$github_url"/compare/staging..."$branch_name
   open $pr_url;
 }
