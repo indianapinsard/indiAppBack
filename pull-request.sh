5405aa22d7c57b8367ade6079bde06b780ce2416
Indi <indiana.pinsard@wattys-conseil.com>
2021-02-26 10:58:25 +0100

    add static folder

--- pull-request.sh
+++ pull-request.sh
@@ -3,7 +3,7 @@ function openpr() {
   github_url=`git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#https://#' -e 's@com:@com/@' -e 's%\.git$%%' | awk '/github/'`;
   branch_name=`git symbolic-ref HEAD | cut -d"/" -f 3,4`;
   pr_url=$github_url"/compare/staging..."$branch_name
-  #open $pr_url;
+  open $pr_url;
 }
 
 # Run git push and then immediately open the Pull Request URL
